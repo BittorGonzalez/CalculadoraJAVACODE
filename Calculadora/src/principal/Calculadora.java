1971f8c946f881becb7b4ba9cb4500d8353e9081
BittorGonzalez <txort2@gmail.com>
2023-02-03 22:04:02 +0100

    Ejercicio JAVACODE y gestion de excepciones

--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -1,10 +1,5 @@
 package principal;
-import java.io.IOException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 import menu.Menu;
 import operaciones.Operaciones;
@@ -21,33 +16,14 @@ public class Calculadora{
 	 * 
 	 * Ejecuta todo el codigo evaluando excepciones aritmeticas
 	 * 
-	 */
-	 private static final Logger LOGGER = Logger.getLogger(Calculadora.class.getName());
-              
+	 */ 
     public static void main(String[] args) {   
         int resultado = 0;
         String operacion = "";
         int[] operandos = new int [2];
         Menu menu = new Menu();
         Operaciones operaciones = new Operaciones();
-        Handler consoleHandler = null;
-		Handler fileHandler = null;
-        
-        try {
-			consoleHandler = new ConsoleHandler();
-			fileHandler = new FileHandler("./operaciones.log");
-			
-			LOGGER.setUseParentHandlers(false);
-			LOGGER.addHandler(consoleHandler);
-			LOGGER.addHandler(fileHandler);
-
-			consoleHandler.setLevel(Level.WARNING);
-			fileHandler.setLevel(Level.ALL);
-			LOGGER.setLevel(Level.ALL);
-			
-		} catch (IOException exception) {
-			LOGGER.log(Level.WARNING, "Ocurrió un error en FileHandler.", exception);
-			}
+    
         
         do{
             operandos = menu.pedirNumeros();
