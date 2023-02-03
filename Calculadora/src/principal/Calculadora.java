6e1d37c382b6d2104484646a74ff95cac3cb87d8
BittorGonzalez <txort2@gmail.com>
2023-02-03 22:36:32 +0100

    Ejercicio JAVACODE y gestion de excepciones

--- Calculadora/src/principal/Calculadora.java
+++ Calculadora/src/principal/Calculadora.java
@@ -1,13 +1,6 @@
 package principal;
 
 
-import java.io.IOException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import menu.Menu;
 import operaciones.Operaciones;
 
@@ -24,32 +17,13 @@ public class Calculadora{
 	 * Ejecuta todo el codigo evaluando excepciones aritmeticas
 	 * 
 	 */ 
-	public static final Logger LOGGER = Logger.getLogger(Calculadora.class.getName());
     public static void main(String[] args) {   
         int resultado = 0;
         String operacion = "";
         int[] operandos = new int [2];
         Menu menu = new Menu();
         Operaciones operaciones = new Operaciones();
-        Handler consoleHandler = null;
-        Handler fileHandler = null;
     
-        try {
-        	
-        	consoleHandler = new ConsoleHandler();
-			fileHandler = new FileHandler("./operaciones.log");
-			
-			LOGGER.setUseParentHandlers(false);
-			LOGGER.addHandler(consoleHandler);
-			LOGGER.addHandler(fileHandler);
-			
-			consoleHandler.setLevel(Level.WARNING);
-			fileHandler.setLevel(Level.ALL);
-			LOGGER.setLevel(Level.ALL);
-        	
-        }catch (IOException e){
-        	LOGGER.log(Level.WARNING, "ERROR en el filehandler", e);
-        }
         
         do{
             operandos = menu.pedirNumeros();
@@ -70,11 +44,9 @@ public class Calculadora{
             		try {
             			resultado = operaciones.dividir(operandos[0], operandos[1]);
                     	System.out.println ("Resultado: " + resultado);
-    					LOGGER.log(Level.FINE, "La ha operacion se ha completado" + resultado);
-
             		}catch (ArithmeticException e){
-    					LOGGER.log(Level.WARNING, "No se puede dividir entre 0", e);
-
+            			System.out.println("ERROR, " + e.getMessage());
+            			System.out.println("No se puede dividir entre 0");
             		}
                 	
             	} else if (operacion.equalsIgnoreCase("%")){
